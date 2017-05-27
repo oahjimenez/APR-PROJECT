4e314ea0ad178b208beedb069dc1d9043d0f78a3
oajimenez <oswaldo.jimenezhidalgo@gmail.com>
2017-05-27 19:43:50 -0400

    Add javafx application

---
+++ javafx/AguaPotableRural/src/aguapotablerural/SQLiteJDBCDriverConnection.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aguapotablerural;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author carlo
+ */
+public class SQLiteJDBCDriverConnection {
+     /**
+     * Connect to a sample database
+     */
+    public static void connect() {
+        Connection conn = null;
+        try {
+            // db parameters
+            String url = "jdbc:sqlite:C:/Users/carlo/Desktop/gitrepo/APR-PROJECT/sqlite3/AGUA_POTABLE_RURAL.db";
+            // create a connection to the database
+            conn = DriverManager.getConnection(url);
+            
+            System.out.println("Connection to SQLite has been established.");
+            
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            try {
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException ex) {
+                System.out.println(ex.getMessage());
+            }
+        }
+    }
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        connect();
+    }
+}

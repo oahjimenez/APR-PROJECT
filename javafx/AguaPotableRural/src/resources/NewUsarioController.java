4626fe554b669b3b0fe2a91c01591a7ed1131674
Sebastian Barahona <oswaldo.jimenezhidalgo@gmail.com>
2017-06-27 20:08:37 -0400

    Add layout templates

---
+++ javafx/AguaPotableRural/src/resources/NewUsarioController.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package resources;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author Sebastián
+ */
+public class NewUsarioController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}

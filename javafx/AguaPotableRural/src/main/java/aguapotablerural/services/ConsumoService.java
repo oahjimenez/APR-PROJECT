d3bf08ef2430c634de07692c63a5af3d5d14e24e
oajimenez <oswaldo.jimenezhidalgo@gmail.com>
2017-10-10 22:21:16 -0300

    Crea servicio ConsumoService para calculo del consumo mensual

---
+++ javafx/AguaPotableRural/src/main/java/aguapotablerural/services/ConsumoService.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main.java.aguapotablerural.services;
+
+import java.time.LocalDate;
+import main.java.aguapotablerural.model.Medidor;
+import main.java.aguapotablerural.model.Usuario;
+
+/**
+ *
+ * @author carlo
+ */
+public class ConsumoService {
+    
+    private static final double CARGO_FIJO_MENSUAL = 1500.00; //todo parametrizar por mes y agregar tabla log de cambio
+    private static final double COSTO_METRO_CUBICO = 500; //todo parametrizar por mes y agregar tabla log de cambio
+    private static final double TOPE_SUBSIDIADO = -1; //todo parametrizar por usuario y mes, agregar tabla log de cambio
+    
+    private final LecturaService lecturaService;
+    
+    
+    public ConsumoService() {
+        this.lecturaService = new LecturaService();
+    }
+    
+    public double getConsumoMensual(Usuario usuario,Medidor medidor, LocalDate mes) {
+        return lecturaService.obtenerLectura(usuario, medidor, mes) - lecturaService.obtenerLectura(usuario, medidor, mes.minusMonths(1));
+    }
+    
+    public double getValorACancelarSinSubsidio(Usuario usuario,Medidor medidor,LocalDate mes) {
+        return CARGO_FIJO_MENSUAL + COSTO_METRO_CUBICO*this.getConsumoMensual(usuario,medidor,mes);
+    }
+    
+    public double getTopeSubsidiado(Usuario usuario,LocalDate mes) {
+        return TOPE_SUBSIDIADO;
+    }
+    
+    public double getPorcentajeSubsidio(Usuario usuario,LocalDate mes) {
+        return -1;
+    }
+    
+    public double getValorACancelarConSubsidio(Usuario usuario,Medidor medidor, LocalDate mes) {
+        double consumoMensual = this.getConsumoMensual(usuario,medidor,mes);
+        double valorACancelarConSubsidio = 0.0;
+        if (consumoMensual < getTopeSubsidiado(usuario,mes)) {
+            valorACancelarConSubsidio = (CARGO_FIJO_MENSUAL + COSTO_METRO_CUBICO*consumoMensual)*(1-getPorcentajeSubsidio(usuario,mes));
+        } else {
+            valorACancelarConSubsidio = ((CARGO_FIJO_MENSUAL + COSTO_METRO_CUBICO*getTopeSubsidiado(usuario,mes))*(1-getPorcentajeSubsidio(usuario,mes)) + (COSTO_METRO_CUBICO*(consumoMensual-getTopeSubsidiado(usuario,mes))));
+        }
+        return valorACancelarConSubsidio;
+    }
+    
+}
